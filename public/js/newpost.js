093f25e780fe4edd74949ce962bcd19f78bff30e
Kyle Canoy <kcanoy@uw.edu>
2021-08-27 11:47:12 -0700

    finish styling and colors

--- public/js/newpost.js
+++ public/js/newpost.js
@@ -4,7 +4,7 @@ async function newPostHandler(event) {
   const title = document.querySelector('input[name="post-title"]').value.trim();
   const content = document.querySelector('input[name="content"]').value.trim();
 
-  const response = await fetch(`api/posts`, {
+  const response = await fetch(`/api/posts`, {
     method: "POST",
     body: JSON.stringify({
       title,
